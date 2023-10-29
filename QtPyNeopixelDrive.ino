4ba548a8105d6782345198d484dcd7073a00f704
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2023-10-29 02:16:28 +0100

    QtPyNeopixelDrive.ino : alternate colors

--- QtPyNeopixelDrive.ino
+++ QtPyNeopixelDrive.ino
@@ -1,25 +1,26 @@
 #include <Adafruit_NeoPixel.h>
 
-// create a pixel strand with 1 pixel on PIN_NEOPIXEL
-//Adafruit_NeoPixel pixels(1, PIN_NEOPIXEL);
+// create a pixel strand with 2 pixels on pin A3, color sequence RGB
 Adafruit_NeoPixel pixels(2, 3, NEO_RGB);
+byte offset;
 
 void setup() {
     pixels.begin();  // initialize the pixel
+    offset= 0;
 }
 
 void loop() {
-    // set the first pixel #0 to red
-    pixels.setPixelColor(0, 0x402000);
-    pixels.setPixelColor(1, 0x002040);
+    pixels.setPixelColor(0^offset, 0x402000);
+    pixels.setPixelColor(1^offset, 0x002040);
     // and write the data
     pixels.show();
   
-    delay(1000);
+    delay(500);
     
     // turn off the pixel
     pixels.clear();
     pixels.show();
   
-    delay(1000);
+    delay(500);
+    offset= 1-offset;
 }
