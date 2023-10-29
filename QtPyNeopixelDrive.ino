a715b2af47093a9dd781957aee188512942fd9b0
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2023-10-29 02:57:42 +0100

    QtPyNeopixelDrive.ino : cycle through gray coded RGB sequence

--- QtPyNeopixelDrive.ino
+++ QtPyNeopixelDrive.ino
@@ -2,25 +2,35 @@
 
 // create a pixel strand with 2 pixels on pin A3, color sequence RGB
 Adafruit_NeoPixel pixels(2, 3, NEO_RGB);
+unsigned long colors[8];
 byte offset;
 
-void setup() {
-    pixels.begin();  // initialize the pixel
-    offset= 0;
+void setup() 
+{
+  byte brightness= 0x20;
+  pixels.begin();  // initialize the pixels
+  for(int c= 0; c<8; c++)
+  {
+    int g;
+    g= c ^ (c>>1);
+    unsigned long color= 0;
+    for(int rgb= 0; rgb<3; rgb++)
+    {
+      color |= (1 & (g >> rgb) ? brightness : 0) << ((2-rgb)<<3); 
+    }
+    colors[c]= color;
+  }
+  offset= 0;
 }
 
-void loop() {
-    pixels.setPixelColor(0^offset, 0x402000);
-    pixels.setPixelColor(1^offset, 0x002040);
-    // and write the data
-    pixels.show();
+void loop() 
+{
+  pixels.setPixelColor(0, colors[offset]);
+  pixels.setPixelColor(1, colors[(offset+1)%8]);
+  // and write the data
+  pixels.show();
+
+  delay(1000);
   
-    delay(500);
-    
-    // turn off the pixel
-    pixels.clear();
-    pixels.show();
-  
-    delay(500);
-    offset= 1-offset;
+  offset= ++offset % 8;
 }
